c38dfa84aefe2b4f5b30720f927c5190c69b4f74
SaiPramod <saipramodkollipara1616@gmail.com>
2025-07-18 13:15:22 +0530

    Temperatur Converter

--- TemperatureConverter/TemperatureConverter.java
+++ TemperatureConverter/TemperatureConverter.java
@@ -1,40 +1,46 @@
-package SaiKetSystems_Internship.Calculator;
+package SaiKetSystems_Internship.TemperatureConverter;
 import java.util.Scanner;
 public class TemperatureConverter {
-    public static  void main(String[] args)
+    public static void main(String[] args)
     {
-        Scanner input=new Scanner(System.in);
-        System.out.println("===Temperature Converter===");
-        System.out.println("1.Celsius to Fahrenheit");
-        System.out.println("2.Fahrenheit to Celsius");
-        System.out.println("Enter your choice(1 or 2):");
-        int choice=input.nextInt();
-        switch (choice)
-        {
+        Scanner input = new Scanner(System.in);
+        System.out.println("===== Temperature Converter =====");
+        System.out.println("Choose conversion type:");
+        System.out.println("1. Celsius to Fahrenheit");
+        System.out.println("2. Fahrenheit to Celsius");
+        System.out.print("Enter your choice (1 or 2): ");
+        int choice = input.nextInt();
+        double inputTemp;
+        double convertedTemp;
+        switch (choice) {
             case 1:
+
             {
-                System.out.println("Enter Celsius value");
-                double inputValue=input.nextInt();
-                double convertedValue=celsiusToFahrenheit(inputValue);
-                System.out.println("Fahrenheit Value of "+inputValue+"C is:"+convertedValue+"F");
+                System.out.print("Enter temperature in Celsius: ");
+                inputTemp = input.nextDouble();
+                convertedTemp = celsiusToFahrenheit(inputTemp);
+                System.out.printf("Fahrenheit temperature of "+inputTemp+"C is"+convertedTemp+"F");
                 break;
             }
             case 2:
             {
-                System.out.println("Enter Fahrenheit value");
-                double inputValue=input.nextInt();
-                double convertedValue=fahrenheitToCelsius(inputValue);
-                System.out.println("Celsius value of "+inputValue+"f is:"+convertedValue+"C");
+                System.out.print("Enter temperature in Fahrenheit: ");
+                inputTemp = input.nextDouble();
+                convertedTemp = fahrenheitToCelsius(inputTemp);
+                System.out.printf("Celsius temperature of "+inputTemp+"F is"+convertedTemp+"C");
+                break;
+            }
+            default:
+            {
+                System.out.println("Invalid choice. Please enter 1 or 2.");
             }
         }
         input.close();
     }
-    public static double celsiusToFahrenheit(double inputValue)
-    {
-        return (inputValue*9/5)+32 ;
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius*9/5)+32;
     }
-    public static double fahrenheitToCelsius(double inputValue)
-    {
-        return (inputValue-32)*5/9;
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit-32)*5/9;
     }
 }
